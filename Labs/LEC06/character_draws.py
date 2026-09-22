ab06fe8d39c8bc46e59be9dd15a95ce1cbe1ae16
junsuk1211 <kiljs1211@gmail.com>
2026-09-23 02:08:38 +0900

    원,사각형,삼각형의 운동으로 이루어지는 전체 흐름 작성

--- Labs/LEC06/character_draws.py
+++ Labs/LEC06/character_draws.py
@@ -1,3 +1,18 @@
 # 실습 과제 진행
+def movecircle():
+    print("원 이동")
+    pass 
+
+def moverectangle():
+    print("사각형 이동")    
+    pass
+
+def movetriangle():
+    print("삼각형 이동")
+    pass
+
 while True:
+    movecircle()
+    moverectangle()
+    movetriangle()
     pass
